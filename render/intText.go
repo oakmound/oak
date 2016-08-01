e313d2da88df24dcb9eeb4e104695045dc549b18
Patrick Stephen <patrick.d.stephen@gmail.com>
2016-08-01 03:59:32 -0500

    More HUD work, health and ammo. A better UI model for the game side, and a text renderable that specifically points to some int pointer

---
+++ render/intText.go
@@ -0,0 +1,47 @@
+package render
+
+import (
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+	"image"
+	"image/draw"
+	"strconv"
+)
+
+type IntText struct {
+	Point
+	Layered
+	text *int
+	d_p  *font.Drawer
+}
+
+func NewIntText(str *int, x, y float64) *IntText {
+	return &IntText{
+		Point: Point{
+			x,
+			y,
+		},
+		text: str,
+		d_p:  d,
+	}
+}
+
+func (t_p *IntText) GetRGBA() *image.RGBA {
+	return nil
+}
+
+func (t_p *IntText) Draw(buff draw.Image) {
+	t_p.d_p.Dot = fixed.P(int(t_p.X), int(t_p.Y))
+	t_p.d_p.DrawString(strconv.Itoa(*t_p.text))
+}
+
+// Center will shift the text so that the existing leftmost point
+// where the text sits becomes the center of the new text.
+func (t_p *IntText) Center() {
+	textWidth := t_p.d_p.MeasureString(strconv.Itoa(*t_p.text)).Round()
+	t_p.ShiftX(float64(-textWidth / 2))
+}
+
+func (t_p *IntText) SetText(str *int) {
+	t_p.text = str
+}
