58f1f813bd8488ced308c99d463bda32106affc6
NateFudenberg <cyreal123@gmail.com>
2016-10-10 21:58:51 -0400

    updated selection to have a non unique selector of x things, used ithis on doodads selection

--- algorithms/selection.go
+++ algorithms/selection.go
@@ -1,6 +1,7 @@
 package algorithms
 
 import (
+	"bitbucket.org/oakmoundstudio/plasticpiston/plastic/dlog"
 	"errors"
 	"math/rand"
 )
@@ -36,4 +37,48 @@ func WeightedChoose(weights []float64, toChoose int) ([]int, error) {
 
 	return out, nil
 
+}
+
+func ChooseX(weights []float64, times int) []int {
+	lengthWeights := len(weights)
+	out := make([]int, times)
+	remainingWeights := make([]float64, lengthWeights)
+	remainingWeights[lengthWeights-1] = weights[lengthWeights-1]
+	for i := lengthWeights - 2; i >= 0; i-- {
+		remainingWeights[i] = remainingWeights[i+1] + weights[i]
+	}
+	for i := 0; i < times; i++ {
+		out[i] = CumWeightedChooseOne(remainingWeights)
+	}
+	return out
+}
+
+func CumWeightedChooseOne(remainingWeights []float64) int {
+	totalWeight := remainingWeights[0]
+	choice := rand.Float64() * totalWeight
+	i := len(remainingWeights) / 2
+	start := 0
+	end := len(remainingWeights) - 1
+	for {
+		dlog.Error("Idx", i)
+		if remainingWeights[i] < choice {
+			if remainingWeights[i-1] < choice {
+				end = i
+				i = (start + end) / 2
+			} else {
+				return i - 1
+			}
+		} else {
+			if i != len(remainingWeights)-1 && remainingWeights[i+1] > choice {
+				start = i
+
+				i = (start + end) / 2
+				if (start+end)%2 == 1 {
+					i++
+				}
+			} else {
+				return i
+			}
+		}
+	}
 }
