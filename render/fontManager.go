4cf6caae7092b9cdd27771a7b5e737dfb3b0f222
Futility <cyreal123@gmail.com>
2017-03-10 21:53:32 -0500

    Added a FontManager for handling fonts in a game

---
+++ render/fontManager.go
@@ -0,0 +1,28 @@
+package render
+
+import "errors"
+
+type FontManager map[string]*Font
+
+func NewFontManager() *FontManager {
+	fm := &FontManager{}
+	fm.NewFont("def", FontGenerator{})
+	return fm
+}
+
+func (fm *FontManager) NewFont(name string, fg FontGenerator) error {
+	manager := (*fm)
+	var err error
+	if _, ok := manager[name]; ok {
+		err = errors.New("Font already existed, overwriting it now.")
+	}
+	manager[name] = (&fg).Generate()
+	return err
+
+}
+
+func (fm *FontManager) Get(elementName string) *Font {
+	manager := (*fm)
+	font, _ := manager[elementName]
+	return font
+}
