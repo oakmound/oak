a4c7c81d8f8079f17446848264c432714119a53b
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-05-20 19:29:40 -0500

    Removed entity/interfaces as they are outdated, unused, and will need more changes in the future to be used anyway. Documented physics

--- physics/physics.go
+++ physics/physics.go
@@ -4,26 +4,34 @@ import (
 	"math"
 )
 
+// A Vector is a two-dimensional point or vector used throughout oak
+// to maintain functionality between packages.
 type Vector struct {
 	X, Y float64
 }
 
 var (
+	// CUTOFF is used for rounding after floating point operations to
+	// zero out vector values that are sufficiently close to zero
 	CUTOFF = 0.001
 )
 
+// NewVector returns a vector with the given x and y components
 func NewVector(x, y float64) Vector {
 	return Vector{x, y}
 }
 
+// Copy copies a Vector
 func (v Vector) Copy() Vector {
 	return NewVector(v.X, v.Y)
 }
 
+// Magnitude returns the magnitude of the combined components of a Vector
 func (v Vector) Magnitude() float64 {
 	return math.Sqrt((v.X * v.X) + (v.Y * v.Y))
 }
 
+// Normalize divides both components in a vector by the vector's magnitude
 func (v Vector) Normalize() Vector {
 	v = v.round()
 	mgn := v.Magnitude()
@@ -35,12 +43,14 @@ func (v Vector) Normalize() Vector {
 	return v
 }
 
+// Zero is shorthand for NewVector(0,0)
 func (v Vector) Zero() Vector {
 	v.X = 0
 	v.Y = 0
 	return v
 }
 
+// Add combines a set of vectors through addition
 func (v Vector) Add(vs ...Vector) Vector {
 	for _, v2 := range vs {
 		v.X += v2.X
@@ -49,6 +59,8 @@ func (v Vector) Add(vs ...Vector) Vector {
 	return v.round()
 }
 
+// Scale scales a vector by a set of floating points
+// Scale(f1,f2,f3) is equivalent to Scale(f1*f2*f3)
 func (v Vector) Scale(fs ...float64) Vector {
 	f2 := 1.0
 	for _, f := range fs {
@@ -59,6 +71,8 @@ func (v Vector) Scale(fs ...float64) Vector {
 	return v.round()
 }
 
+// Rotate takes in a set of angles and rotates v by their sum
+// the input angles are assumed to be in degrees.
 func (v Vector) Rotate(fs ...float64) Vector {
 	angle := 0.0
 	for _, f := range fs {
@@ -72,18 +86,20 @@ func (v Vector) Rotate(fs ...float64) Vector {
 	return v.round()
 }
 
-// Defaults to Degrees
+// Angle returns this vector as an angle in degrees
 func (v Vector) Angle() float64 {
 	return math.Atan2(v.Y, v.X) * 180 / math.Pi
 }
 
+// Dot returns the dot product of the vectors
 func (v Vector) Dot(v2 Vector) float64 {
 	x := v.X * v2.X
 	y := v.Y * v2.Y
 	return x + y
 }
 
-// Distance is Euclidean
+// Distance on two vectors returns the euclidean distance
+// from v to v2
 func (v Vector) Distance(v2 Vector) float64 {
 	return v.Add(v2.Scale(-1)).Magnitude()
 }
@@ -98,27 +114,36 @@ func (v Vector) round() Vector {
 	return v
 }
 
+// ShiftX is equivalent to v.X += x
 func (v Vector) ShiftX(x float64) Vector {
 	v.X += x
 	return v
 }
+
+// ShiftY is equivalent to v.Y += y
 func (v Vector) ShiftY(y float64) Vector {
 	v.Y += y
 	return v
 }
+
+// GetX returns v.X
 func (v Vector) GetX() float64 {
 	return v.X
 }
+
+// GetY returns v.Y
 func (v Vector) GetY() float64 {
 	return v.Y
 }
 
+// SetPos is equivalent to NewVector(x,y)
 func (v Vector) SetPos(x, y float64) Vector {
 	v.X = x
 	v.Y = y
 	return v
 }
 
+// GetPos returns both v.X and v.Y
 func (v Vector) GetPos() (float64, float64) {
 	return v.X, v.Y
 }
