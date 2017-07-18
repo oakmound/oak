b3111c2b86d2293905427382ea5198c9b81e327c
Futility <cyreal123@gmail.com>
2017-07-17 20:42:30 -0400

    Adding render and particle doc.go files for godoc.

--- render/particle/doc.go
+++ render/particle/doc.go
@@ -1,9 +1,6 @@
+//Package particle provides options for generating renderable particle sources.
+
+//Particles are created via generators which are in turn managed via sources.
+//Due to particles often being used in high volumes they have their own id spaces and special rules for collision.
+
 package particle
-
-/*
-Package particle provides options for generating renderable particle sources.
-
-Particles are created via generators which are in turn managed via sources.
-Particles have a variety of collision and presentation options all whih can be found in this package.
-
-*/
