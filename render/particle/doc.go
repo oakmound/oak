ea38fc173316866a243d1756eaf62fd4848b0738
Futility <cyreal123@gmail.com>
2017-07-17 20:28:18 -0400

    Testing a doc.go file for particle package. Per godoc documentation this is the accepted way to set overview comments.

---
+++ render/particle/doc.go
@@ -0,0 +1,9 @@
+package particle
+
+/*
+Package particle provides options for generating renderable particle sources.
+
+Particles are created via generators which are in turn managed via sources.
+Particles have a variety of collision and presentation options all whih can be found in this package.
+
+*/
