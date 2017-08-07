c105ae4993e27fbd8b93e9fba1f34be5c37c10e1
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-08-07 01:52:02 -0500

    testing coverage through not coveralls

---
+++ cover.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+set -e
+echo "" > coverage.txt
+go test -coverprofile=profile.out -covermode=atomic .
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./shape
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./timing
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./physics
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./event
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./render
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./render/particle
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./alg
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./alg/intgeom
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./collision
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./fileutil
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
+go test -coverprofile=profile.out -covermode=atomic ./mouse
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
