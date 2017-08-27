0d37c9caa650898dda5d02a710751a1e3f3424bc
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-08-27 00:29:19 -0500

    Some minimal error tests.

---
+++ oakerr/errors_test.go
@@ -0,0 +1,17 @@
+package oakerr
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrorsAreErrors(t *testing.T) {
+
+	var err error
+	err = NotLoadedError{}
+	assert.NotEmpty(t, err.Error())
+	err = ExistingFontError{}
+	assert.NotEmpty(t, err.Error())
+	// Assert nothing crashed
+}
