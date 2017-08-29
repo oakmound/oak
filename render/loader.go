dcc95072b062e0733beab35050e3b6dfe2c3133c
StephenPatrick <sythe2o0@gmail.com>
2017-08-29 17:47:08 -0500

    Split BatchLoad up into a few parts

--- render/loader.go
+++ render/loader.go
@@ -22,6 +22,12 @@ import (
 var (
 	regexpSingleNumber, _ = regexp.Compile(`^\d+$`)
 	regexpTwoNumbers, _   = regexp.Compile(`^\d+x\d+$`)
+	supportedFileEndings  = map[string]bool{
+		"jpeg": true,
+		".jpg": true,
+		".gif": true,
+		".png": true,
+	}
 )
 
 var (
@@ -216,62 +222,25 @@ func BatchLoad(baseFolder string) error {
 		dlog.Error(err)
 		return err
 	}
-	aliasFile, err := fileutil.ReadFile(filepath.Join(baseFolder, "alias.json"))
-	aliases := make(map[string]string)
-	if err == nil {
-		err = json.Unmarshal(aliasFile, &aliases)
-		if err != nil {
-			dlog.Error("Alias file unparseable: ", err)
-		}
-	}
+	aliases := parseAliasFile(baseFolder)
 
 	for i, folder := range folders {
 
 		dlog.Verb("folder ", i, folder.Name())
 		if folder.IsDir() {
 
-			var frameW int
-			var frameH int
-
-			if folder.Name() == "raw" {
-				frameW = 0
-				frameH = 0
-			} else if result := regexpTwoNumbers.Find([]byte(folder.Name())); result != nil {
-				vals := strings.Split(string(result), "x")
-				dlog.Verb("Extracted dimensions: ", vals)
-				frameW, _ = strconv.Atoi(vals[0])
-				frameH, _ = strconv.Atoi(vals[1])
-			} else if result := regexpSingleNumber.Find([]byte(folder.Name())); result != nil {
-				val, _ := strconv.Atoi(string(result))
-				frameW = val
-				frameH = val
-			} else {
-				if aliased, ok := aliases[folder.Name()]; ok {
-					if result := regexpTwoNumbers.Find([]byte(aliased)); result != nil {
-						vals := strings.Split(string(result), "x")
-						dlog.Verb("Extracted dimensions: ", vals)
-						frameW, _ = strconv.Atoi(vals[0])
-						frameH, _ = strconv.Atoi(vals[1])
-					} else if result := regexpSingleNumber.Find([]byte(aliased)); result != nil {
-						val, _ := strconv.Atoi(string(result))
-						frameW = val
-						frameH = val
-					} else {
-						return errors.New("Alias value not parseable as a frame width and height pair")
-					}
-				} else {
-					return errors.New("Alias name not found in alias file")
-				}
+			frameW, frameH, err := parseLoadFolderName(aliases, folder.Name())
+			if err != nil {
+				return err
 			}
 
 			files, _ := fileutil.ReadDir(filepath.Join(baseFolder, folder.Name()))
 			for _, file := range files {
 				if !file.IsDir() {
-					n := file.Name()
-					switch n[len(n)-4:] {
-					case ".png":
-						dlog.Verb("loading file ", n)
-						buff, err := loadImage(baseFolder, filepath.Join(folder.Name(), n))
+					name := file.Name()
+					if _, ok := supportedFileEndings[name[len(name)-4:]]; ok {
+						dlog.Verb("loading file ", name)
+						buff, err := loadImage(baseFolder, filepath.Join(folder.Name(), name))
 						if err != nil {
 							dlog.Error(err)
 							continue
@@ -284,27 +253,73 @@ func BatchLoad(baseFolder string) error {
 						if frameW == 0 || frameH == 0 {
 							continue
 						} else if w < frameW || h < frameH {
-							dlog.Error("File ", n, " in folder", folder.Name(), " is too small for these folder dimensions")
+							dlog.Error("File ", name, " in folder", folder.Name(), " is too small for folder dimensions", frameW, frameH)
 							return errors.New("File in folder is too small for these folder dimensions")
 
 							// Load this as a sheet if it is greater
 							// than the folder size's frame size
 						} else if w != frameW || h != frameH {
 							dlog.Verb("Loading as sprite sheet")
-							_, err = LoadSheet(baseFolder, filepath.Join(folder.Name(), n), frameW, frameH, defaultPad)
+							_, err = LoadSheet(baseFolder, filepath.Join(folder.Name(), name), frameW, frameH, defaultPad)
 							if err != nil {
 								dlog.Error(err)
 							}
 						}
-					default:
-						dlog.Error("Unsupported file ending for batchLoad: ", n)
+					} else {
+						dlog.Error("Unsupported file ending for batchLoad: ", name)
 					}
 				}
 			}
 		} else {
-			dlog.Verb("Not Folder")
+			dlog.Verb("Not Folder", folder.Name())
 		}
-
 	}
 	return nil
+}
+
+func parseAliasFile(baseFolder string) map[string]string {
+	aliasFile, err := fileutil.ReadFile(filepath.Join(baseFolder, "alias.json"))
+	aliases := make(map[string]string)
+	if err == nil {
+		err = json.Unmarshal(aliasFile, &aliases)
+		if err != nil {
+			dlog.Error("Alias file unparseable: ", err)
+		}
+	}
+	return aliases
+}
+
+func parseLoadFolderName(aliases map[string]string, name string) (int, int, error) {
+	var frameW, frameH int
+	if name == "raw" {
+		frameW = 0
+		frameH = 0
+	} else if result := regexpTwoNumbers.Find([]byte(name)); result != nil {
+		vals := strings.Split(string(result), "x")
+		dlog.Verb("Extracted dimensions: ", vals)
+		frameW, _ = strconv.Atoi(vals[0])
+		frameH, _ = strconv.Atoi(vals[1])
+	} else if result := regexpSingleNumber.Find([]byte(name)); result != nil {
+		val, _ := strconv.Atoi(string(result))
+		frameW = val
+		frameH = val
+	} else {
+		if aliased, ok := aliases[name]; ok {
+			if result := regexpTwoNumbers.Find([]byte(aliased)); result != nil {
+				vals := strings.Split(string(result), "x")
+				dlog.Verb("Extracted dimensions: ", vals)
+				frameW, _ = strconv.Atoi(vals[0])
+				frameH, _ = strconv.Atoi(vals[1])
+			} else if result := regexpSingleNumber.Find([]byte(aliased)); result != nil {
+				val, _ := strconv.Atoi(string(result))
+				frameW = val
+				frameH = val
+			} else {
+				return 0, 0, errors.New("Alias value not parseable as a frame width and height pair")
+			}
+		} else {
+			return 0, 0, errors.New("Alias name not found in alias file")
+		}
+	}
+	return frameW, frameH, nil
 }
