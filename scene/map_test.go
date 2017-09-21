f940a28c89a261313664171f3a192c42d83bbb38
StephenPatrick <sythe2o0@gmail.com>
2017-09-21 18:42:21 -0500

    Finish scene tests

--- scene/map_test.go
+++ scene/map_test.go
@@ -1,6 +1,7 @@
 package scene
 
 import (
+	"image"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,16 @@ func TestMap(t *testing.T) {
 	assert.NotNil(t, m.Add("test", nil, nil, nil))
 	_, ok = m.Get("test")
 	assert.True(t, ok)
+	m.CurrentScene = "test"
+	_, ok = m.GetCurrent()
+	assert.True(t, ok)
+}
+
+func TestTransition(t *testing.T) {
+	fadeFn := Fade(1, 10)
+	assert.False(t, fadeFn(nil, 11))
+	assert.True(t, fadeFn(image.NewRGBA(image.Rect(0, 0, 50, 50)), 2))
+	zoomFn := Zoom(.5, .5, 10, .1)
+	assert.False(t, zoomFn(nil, 11))
+	assert.True(t, zoomFn(image.NewRGBA(image.Rect(0, 0, 50, 50)), 2))
 }
