9ef0e9128509c277dcdf049f65c4a79bfe111f09
StephenPatrick <sythe2o0@gmail.com>
2017-10-24 21:02:03 -0500

    Hack outlines to provide a 4-directional outline

--- shape/outline.go
+++ shape/outline.go
@@ -43,78 +43,97 @@ var (
 
 // ToOutline returns the set of points along the input shape's outline, if
 // one exists.
+// todo: support 8-directional or 4-directional outlines
 func ToOutline(shape Shape) func(...int) ([]intgeom.Point, error) {
 	return func(sizes ...int) ([]intgeom.Point, error) {
-		w := sizes[0]
-		h := sizes[0]
-		if len(sizes) > 1 {
-			h = sizes[1]
+		return toOutline(shape, 1, sizes...)
+	}
+}
+
+// this is a hack to support 4 and 8 directional outlines
+func toOutline(shape Shape, dirInc int, sizes ...int) ([]intgeom.Point, error) {
+	w := sizes[0]
+	h := sizes[0]
+	if len(sizes) > 1 {
+		h = sizes[1]
+	}
+
+	//First decrement on diagonal to find start of outline
+	startX := 0.0
+	startY := 0.0
+	fw := float64(w)
+	fh := float64(h)
+	maxDim := fw
+	if h > w {
+		maxDim = fh
+	}
+	xDelta := fw / maxDim
+	yDelta := fh / maxDim
+	for !shape.In(int(startX), int(startY), sizes...) {
+		startX += xDelta
+		startY += yDelta
+		if startX >= fw || startY >= fh {
+			return []intgeom.Point{}, errors.New("Could not find any valid space on the shapes diagonal... Assuming that it is not valid for outlines")
 		}
+	}
 
-		//First decrement on diagonal to find start of outline
-		startX := 0.0
-		startY := 0.0
-		fw := float64(w)
-		fh := float64(h)
-		maxDim := fw
-		if h > w {
-			maxDim = fh
-		}
-		xDelta := fw / maxDim
-		yDelta := fh / maxDim
-		for !shape.In(int(startX), int(startY), sizes...) {
-			startX += xDelta
-			startY += yDelta
-			if startX >= fw || startY >= fh {
-				return []intgeom.Point{}, errors.New("Could not find any valid space on the shapes diagonal... Assuming that it is not valid for outlines")
-			}
-		}
+	for startY >= 0 && shape.In(int(startX), int(startY), sizes...) {
+		startY--
+	}
+	startY++
 
-		for startY >= 0 && shape.In(int(startX), int(startY), sizes...) {
-			startY--
-		}
-		startY++
+	//Here we have found a point on the outline
+	sx := int(startX)
+	sy := int(startY)
+	x := sx
+	y := sy
 
-		//Here we have found a point on the outline
-		sx := int(startX)
-		sy := int(startY)
-		x := sx
-		y := sy
+	outline := []intgeom.Point{intgeom.NewPoint(x, y)}
 
-		outline := []intgeom.Point{intgeom.NewPoint(x, y)}
+	direction := topright
+	for i := 1; i < dirInc; i++ {
+		direction++
+	}
 
-		direction := topright
+	x += xyMods[direction*2]
+	y += xyMods[direction*2+1]
 
-		x += xyMods[direction*2]
-		y += xyMods[direction*2+1]
-
-		for direction != top && !inOutline(shape, x, y, w, h) {
+	for direction != top && !inOutline(shape, x, y, w, h) {
+		for i := 0; i < dirInc; i++ {
 			x += pointDeltas[direction*2]
 			y += pointDeltas[direction*2+1]
 			direction = (direction + 1) % lastdirection
 		}
-		if direction == top {
-			return outline, nil
-		}
+	}
+	if direction == top {
+		return outline, nil
+	}
 
-		//Follow the outline point by point
-		for x != sx || y != sy {
-			outline = append(outline, intgeom.NewPoint(x, y))
-			direction -= 2
-			if direction < 0 {
-				direction += lastdirection
-			}
-			x += xyMods[direction*2]
-			y += xyMods[direction*2+1]
-			//From a point on the outline look clockwise around for next direction
-			for !inOutline(shape, x, y, w, h) {
+	//Follow the outline point by point
+	for x != sx || y != sy {
+		outline = append(outline, intgeom.NewPoint(x, y))
+		direction -= 2
+		if direction < 0 {
+			direction += lastdirection
+		}
+		x += xyMods[direction*2]
+		y += xyMods[direction*2+1]
+		//From a point on the outline look clockwise around for next direction
+		for !inOutline(shape, x, y, w, h) {
+			for i := 0; i < dirInc; i++ {
 				x += pointDeltas[direction*2]
 				y += pointDeltas[direction*2+1]
 				direction = (direction + 1) % lastdirection
 			}
 		}
+	}
 
-		return outline, nil
+	return outline, nil
+}
+
+func ToOutline4(shape Shape) func(...int) ([]intgeom.Point, error) {
+	return func(sizes ...int) ([]intgeom.Point, error) {
+		return toOutline(shape, 2, sizes...)
 	}
 }
 
