85a13919800160ac9e7966f34dbe6019f7086394
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-01-27 17:51:49 -0600

    Add top down shooter examples (unfinished)

---
+++ examples/top-down-shooter-tutorial/shooting/shooting.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"image/color"
+	"time"
+
+	"github.com/oakmound/oak"
+	"github.com/oakmound/oak/collision"
+	"github.com/oakmound/oak/entities"
+	"github.com/oakmound/oak/event"
+	"github.com/oakmound/oak/key"
+	"github.com/oakmound/oak/mouse"
+	"github.com/oakmound/oak/physics"
+	"github.com/oakmound/oak/render"
+	"github.com/oakmound/oak/scene"
+)
+
+const (
+	Enemy  collision.Label = 1
+	Player collision.Label = 2
+)
+
+var (
+	playerAlive = true
+)
+
+func main() {
+	oak.Add("tds", func(string, interface{}) {
+		playerAlive = true
+		char := entities.NewMoving(100, 100, 32, 32,
+			render.NewColorBox(32, 32, color.RGBA{0, 255, 0, 255}),
+			nil, 0, 0)
+
+		char.Speed = physics.NewVector(5, 5)
+		render.Draw(char.R)
+
+		char.Bind(func(id int, _ interface{}) int {
+			char := event.GetEntity(id).(*entities.Moving)
+			char.Delta.Zero()
+			if oak.IsDown(key.W) {
+				char.Delta.ShiftY(-char.Speed.Y())
+			}
+			if oak.IsDown(key.A) {
+				char.Delta.ShiftX(-char.Speed.X())
+			}
+			if oak.IsDown(key.S) {
+				char.Delta.ShiftY(char.Speed.Y())
+			}
+			if oak.IsDown(key.D) {
+				char.Delta.ShiftX(char.Speed.X())
+			}
+			char.ShiftPos(char.Delta.X(), char.Delta.Y())
+			hit := char.HitLabel(Enemy)
+			if hit != nil {
+				playerAlive = false
+			}
+
+			return 0
+		}, event.Enter)
+
+		char.Bind(func(id int, me interface{}) int {
+			char := event.GetEntity(id).(*entities.Moving)
+			mevent := me.(mouse.Event)
+			render.DrawForTime(
+				render.NewLine(char.X()+char.W/2, char.Y()+char.H/2, mevent.X(), mevent.Y(), color.RGBA{0, 128, 0, 128}),
+				1,
+				time.Millisecond*50)
+			return 0
+		}, mouse.Press)
+
+	}, func() bool {
+		return playerAlive
+	}, func() (string, *scene.Result) {
+		return "tds", nil
+	})
+	oak.Init("tds")
+}
