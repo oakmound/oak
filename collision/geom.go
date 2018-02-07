57eb268366d6dde11476e3c4a86f1c6927509c6f
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-02-07 03:12:19 -0600

    Add rtee-LICENSE

--- collision/geom.go
+++ collision/geom.go
@@ -1,6 +1,6 @@
 // Copyright 2012 Daniel Connelly.  All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the rtree-LICENSE file.
 
 package collision
 
--- collision/rtree_test.go
+++ collision/rtree_test.go
@@ -1,3 +1,7 @@
+// Copyright 2012 Daniel Connelly.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the rtree-LICENSE file.
+
 package collision
 
 import (
