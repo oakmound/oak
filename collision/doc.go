09be666e827a7942f3a25f9d799aafc6076d282d
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-02-07 06:46:50 -0600

    Package doc updating

--- collision/doc.go
+++ collision/doc.go
@@ -1,3 +1,3 @@
-// Package collision provides collision tree and space structures, raycasting
-// utilities and hit detection functions on spaces.
+// Package collision provides collision tree and space structures along with
+// hit detection functions on spaces.
 package collision
