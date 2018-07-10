dd47dcd0580f5499e931b68ec0f460c175def366
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-07-09 20:08:36 -0500

    Add floatgeom.Point4

--- alg/floatgeom/point.go
+++ alg/floatgeom/point.go
@@ -12,6 +12,9 @@ type Point2 [2]float64
 // Point3 represents a 3D point in space.
 type Point3 [3]float64
 
+// Point4 represents a 4D point, in space + some additional dimension.
+type Point4 [4]float64
+
 // AnglePoint creates a unit vector from the given angle in degrees as a Point2.
 func AnglePoint(angle float64) Point2 {
 	return RadianPoint(angle * math.Pi / 180)
@@ -33,6 +36,13 @@ func (p Point2) Dim(i int) float64 {
 // Panics if i > 2. No check is made for efficiency's sake, pending benchmarks,
 // but adding an error here would significantly worsen the API.
 func (p Point3) Dim(i int) float64 {
+	return p[i]
+}
+
+// Dim returns the value of p in the ith dimension.
+// Panics if i > 3. No check is made for efficiency's sake, pending benchmarks,
+// but adding an error here would significantly worsen the API.
+func (p Point4) Dim(i int) float64 {
 	return p[i]
 }
 
@@ -59,6 +69,26 @@ func (p Point3) Y() float64 {
 // Z returns p's value on the Z axis.
 func (p Point3) Z() float64 {
 	return p.Dim(2)
+}
+
+// X returns p's value on the X axis.
+func (p Point4) X() float64 {
+	return p.Dim(0)
+}
+
+// Y returns p's value on the Y axis.
+func (p Point4) Y() float64 {
+	return p.Dim(1)
+}
+
+// Z returns p's value on the Z axis.
+func (p Point4) Z() float64 {
+	return p.Dim(2)
+}
+
+// W returns p's value on the W axis.
+func (p Point4) W() float64 {
+	return p.Dim(3)
 }
 
 // Distance calculates the distance between this Point2 and another.
