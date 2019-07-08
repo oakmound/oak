4e71764841f02996fca9712c984851c9b3b79814
Futility <cyreal123@gmail.com>
2019-07-08 19:03:31 -0400

    Additional render tests

--- render/drawStack_test.go
+++ render/drawStack_test.go
@@ -1,14 +1,23 @@
 package render
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestDrawStack(t *testing.T) {
 	resetDraw = true
 	PreDraw()
 	resetDraw = false
 	PreDraw()
+	assert.Equal(t, 1, len(GlobalDrawStack.as))
 	SetDrawStack(
 		NewHeap(false),
 		NewDrawFPS(),
 	)
+	assert.Equal(t, 2, len(GlobalDrawStack.as))
+	GlobalDrawStack.Pop()
+	PreDraw()
+	assert.Equal(t, 1, len(GlobalDrawStack.as))
 }
