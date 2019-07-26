6157b8113847605690e8eab9a5db38cd57a85cd2
Patrick Stephen <patrick.d.stephen@gmail.com>
2019-07-26 18:24:36 -0500

    Move more scene tests to oak

--- doc_test.go
+++ doc_test.go
@@ -23,7 +23,6 @@ func Example() {
 		return "basicScene", nil
 	})
 	Init("basicScene")
-
 }
 
 func ExampleAdd() {
@@ -53,3 +52,24 @@ func ExampleAddCommand() {
 	})
 }
  
+func getBasicScene() scene.Scene {
+	return scene.Scene{func(string, interface{}) {
+		char := entities.NewMoving(100, 100, 16, 32,
+			render.NewColorBox(16, 32, color.RGBA{255, 0, 0, 255}),
+			nil, 0, 0)
+		render.Draw(char.R)
+
+	}, func() bool {
+		return true
+	}, func() (string, *scene.Result) {
+		return "basicScene", nil
+	},
+	}
+}
+
+// Addscene lets a central package manage a set of scenes across subpackages such as in weekly87
+// Note the example wont work because there is nothing
+func ExampleAddScene() {
+	AddScene("scene1", getBasicScene())
+	AddScene("scene2", getBasicScene())
+}
---
+++ scene/example_test.go
@@ -0,0 +1,27 @@
+package scene_test
+
+import (
+	"fmt"
+
+	"github.com/oakmound/oak/scene"
+)
+
+func ExampleMap_GetCurrent() {
+	m := scene.NewMap()
+	sc := scene.Scene{
+		Start: func(prevScene string, data interface{}) {
+			fmt.Println("Starting screen one")
+		},
+	}
+	m.AddScene("screen1", sc)
+	m.CurrentScene = "screen2"
+	_, ok := m.GetCurrent()
+	if !ok {
+		fmt.Println("Screen two did not exist")
+	}
+	m.CurrentScene = "screen1"
+	sc, ok = m.GetCurrent()
+	sc.Start("screen0", nil)
+	// Output: Screen two did not exist
+	// Starting screen one
+}
