c68576ba2a8756264c55eeaacee52621bcc835a8
Patrick Stephen <patrick.d.stephen@gmail.com>
2020-12-05 12:05:08 -0600

    Remove four character requirement from image decoders
    This was an artifact from before using filepath.Ext. All lengths are properly supported now.

--- render/decoder.go
+++ render/decoder.go
@@ -19,11 +19,11 @@ type Decoder func(io.Reader) (image.Image, error)
 
 var (
 	fileDecoders = map[string]Decoder{
-		"jpeg": jpeg.Decode,
-		".jpg": jpeg.Decode,
-		".gif": gif.Decode,
-		".png": png.Decode,
-		".bmp": bmp.Decode,
+		".jpeg": jpeg.Decode,
+		".jpg":  jpeg.Decode,
+		".gif":  gif.Decode,
+		".png":  png.Decode,
+		".bmp":  bmp.Decode,
 	}
 )
 
@@ -31,12 +31,16 @@ var (
 // for file loading. If the extension string is already set,
 // the existing decoder will not be overwritten.
 func RegisterDecoder(ext string, decoder Decoder) error {
-	if len(ext) != 4 {
-		return oakerr.InvalidLength{
-			InputName:      "ext",
-			RequiredLength: 4,
-			Length:         len(ext),
+	_, ok := fileDecoders[ext]
+	if ok {
+		return oakerr.ExistingElement{
+			InputName:   "ext",
+			InputType:   "string",
+			Overwritten: false,
 		}
+		}
+	fileDecoders[ext] = decoder
+	return nil
 	}
 	_, ok := fileDecoders[ext]
 	if ok {
