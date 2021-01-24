cdb28cf4335fa829b9ab040554c34bf3ac76f641
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-01-24 14:21:23 -0600

    Update decoder tests for more lenient extension registration

--- render/decoder_test.go
+++ render/decoder_test.go
@@ -7,9 +7,6 @@ import (
 )
 
 func TestRegisterDecoder(t *testing.T) {
-	assert.NotNil(t, RegisterDecoder("too long", nil))
-	short := "s"
-	assert.NotNil(t, RegisterDecoder(short, nil))
 	assert.NotNil(t, RegisterDecoder(".png", nil))
 	assert.Nil(t, RegisterDecoder(".new", nil))
 }
