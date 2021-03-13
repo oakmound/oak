4e5ec2071bb07b02129742428d159665acd7a59d
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-03-13 16:15:48 -0600

    Ensure timer in timing.DoAfter is cleaned up

--- timing/delay.go
+++ timing/delay.go
@@ -6,15 +6,17 @@ import (
 )
 
 var (
-	// ClearDelayCh is used to stop all ongoing delays
+	// ClearDelayCh is used to stop all ongoing delays. It should not be closed.
 	ClearDelayCh = make(chan bool)
 )
 
 // DoAfter wraps time calls in a select that will stop events from happening
 // when ClearDelayCh pulls
 func DoAfter(d time.Duration, f func()) {
+	t := time.NewTimer(d)
+	defer t.Stop()
 	select {
-	case <-time.After(d):
+	case <-t.C:
 		f()
 	case <-ClearDelayCh:
 	}
