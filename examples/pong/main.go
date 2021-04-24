4f3ae7bb8aaa5dae7c808dd10412349275344f6c
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-04-24 10:33:39 -0500

    examples/pong: make the example look more like pong, raise fps

--- examples/pong/main.go
+++ examples/pong/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"image/color"
+	"math"
 	"math/rand"
 
 	oak "github.com/oakmound/oak/v2"
 	"github.com/oakmound/oak/v2/collision"
 	"github.com/oakmound/oak/v2/entities"
 	"github.com/oakmound/oak/v2/event"
+	"github.com/oakmound/oak/v2/key"
 	"github.com/oakmound/oak/v2/render"
 	"github.com/oakmound/oak/v2/scene"
 )
@@ -18,37 +20,35 @@ var (
 )
 
 const (
-	paddle collision.Label = 1
+	hitPaddle collision.Label = 1
 )
 
 func main() {
 	oak.AddScene("pong",
 		scene.Scene{Start: func(*scene.Context) {
 			newPaddle(20, 200, 1)
-			newPaddle(590, 200, 2)
+			newPaddle(600, 200, 2)
 			newBall(320, 240)
-			render.Draw(render.DefFont().NewIntText(&score2, 200, 20), 3)
-			render.Draw(render.DefFont().NewIntText(&score1, 400, 20), 3)
+			render.Draw(render.DefaultFont().NewIntText(&score2, 200, 20), 3)
+			render.Draw(render.DefaultFont().NewIntText(&score1, 400, 20), 3)
 		}})
+	oak.SetupConfig.DrawFrameRate = 120
 	oak.Init("pong")
 }
 
-// Todo: this was written before we had vectors, then changed at release 1.0 to be a lot
-// more verbose once we had vectors, but we'd really like it to not be so
-// wordy.
 func newBall(x, y float64) {
-	b := entities.NewMoving(x, y, 10, 10, render.NewColorBox(10, 10, color.RGBA{0, 255, 0, 255}), nil, 0, 0)
+	b := entities.NewMoving(x, y, 10, 10, render.NewColorBox(10, 10, color.RGBA{255, 255, 255, 255}), nil, 0, 0)
 	render.Draw(b.R, 2)
 	b.Bind(event.Enter, func(id event.CID, nothing interface{}) int {
 		if b.Delta.X() == 0 && b.Delta.Y() == 0 {
 			b.Delta.SetY((rand.Float64() - 0.5) * 4)
 			b.Delta.SetX((rand.Float64() - 0.5) * 16)
-			if b.Delta.X() == 0 {
+			if math.Abs(b.Delta.X()) < 0.1 {
 				b.Delta.SetX(8)
 			}
 		}
 		b.ShiftPos(b.Delta.X(), b.Delta.Y())
-		if collision.HitLabel(b.Space, paddle) != nil {
+		if collision.HitLabel(b.Space, hitPaddle) != nil {
 			b.Delta.SetX(-1.1 * b.Delta.X())
 			b.Delta.SetY(b.Delta.Y() + (rand.Float64()-0.5)*8)
 		}
@@ -69,21 +69,20 @@ func newBall(x, y float64) {
 }
 
 func newPaddle(x, y float64, player int) {
-	p := entities.NewMoving(x, y, 20, 100, render.NewColorBox(20, 100, color.RGBA{255, 0, 0, 255}), nil, 0, 0)
-	p.Speed.SetY(4)
+	p := entities.NewMoving(x, y, 20, 100, render.NewColorBox(20, 100, color.RGBA{255, 255, 255, 255}), nil, 0, 0)
+	p.Speed.SetY(8)
 	render.Draw(p.R, 1)
-	p.Space.UpdateLabel(paddle)
+	p.Space.UpdateLabel(hitPaddle)
 	if player == 1 {
-		p.Bind(event.Enter, enterPaddle("UpArrow", "DownArrow"))
+		p.Bind(event.Enter, enterPaddle(key.UpArrow, key.DownArrow))
 	} else {
-		p.Bind(event.Enter, enterPaddle("W", "S"))
+		p.Bind(event.Enter, enterPaddle(key.W, key.S))
 	}
-	p.SetPos(x, y)
 }
 
 func enterPaddle(up, down string) func(event.CID, interface{}) int {
 	return func(id event.CID, nothing interface{}) int {
-		p := event.GetEntity(id).(*entities.Moving)
+		p := id.E().(*entities.Moving)
 		p.Delta.SetY(0)
 		if oak.IsDown(up) {
 			p.Delta.SetY(-p.Speed.Y())
