5189ff1a0f6d92b4a49fc4dfe975193d524c34f4
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-05-24 07:04:26 -0500

    examples: add svg example

---
+++ examples/svg/go.mod
@@ -0,0 +1,12 @@
+module github.com/oakmound/oak/examples/svg
+
+go 1.16
+
+require (
+	github.com/oakmound/oak/v3 v3.0.0-alpha.1
+	github.com/srwiley/oksvg v0.0.0-20210320200257-875f767ac39a
+	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9	
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/image v0.0.0-20210504121937-7319ad40d33e // indirect
+	golang.org/x/net v0.0.0-20210521195947-fe42d452be8f // indirect
+)
---
+++ examples/svg/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"image"
+
+	"github.com/oakmound/oak/v3"
+	"github.com/oakmound/oak/v3/render"
+	"github.com/oakmound/oak/v3/scene"
+	"github.com/srwiley/oksvg"
+	"github.com/srwiley/rasterx"
+)
+
+func main() {
+	oak.AddScene("svg", scene.Scene{
+		Start: func(*scene.Context) {
+			// load svg
+			// svg from oksvg testdata
+			icon, err := oksvg.ReadIcon("./assets/images/TestShapes.svg")
+			if err != nil {
+				fmt.Println(err)
+			}
+			// put it in the thing
+
+			inputW, inputH := icon.ViewBox.W, icon.ViewBox.H
+			iconAspect := inputW / inputH
+			const width = 640
+			const height = 480
+
+			buff := image.NewRGBA(image.Rect(0, 0, width, height))
+
+			viewAspect := float64(width) / float64(height)
+			outputW, outputH := width, height
+			if viewAspect > iconAspect {
+				outputW = int(float64(height) * iconAspect)
+			} else if viewAspect < iconAspect {
+				outputH = int(float64(width) / iconAspect)
+			}
+			scanner := rasterx.NewScannerGV(int(inputW), int(inputH), buff, image.Rect(0, 0, width, height))
+			scanner.SetBounds(10000, 10000)
+			dasher := rasterx.NewDasher(width, height, scanner)
+			icon.SetTarget(0, 0, float64(outputW), float64(outputH))
+			icon.Draw(dasher, 1)
+
+			render.Draw(render.NewSprite(0, 0, buff))
+		},
+	})
+
+	oak.Init("svg")
+}
