60181f7e18e61bb3da44a5a87d92327f3b793f8c
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-05-29 21:19:21 -0500

    oak: lazily initialize the default controller
    This is significant for the debug stream because otherwise multi window scopes show up as 2,3 instead of 1,2

--- default.go
+++ default.go
@@ -2,6 +2,7 @@ package oak
 
 import (
 	"image"
+	"sync"
 	"time"
 
 	"github.com/oakmound/oak/v3/alg/intgeom"
@@ -10,82 +11,109 @@ import (
 	"github.com/oakmound/oak/v3/scene"
 )
 
-var defaultController = NewController()
+var defaultController *Controller
+
+var initDefaultControllerOnce sync.Once
+
+func initDefaultController() {
+	initDefaultControllerOnce.Do(func() {
+		defaultController = NewController()
+	})
+}
 
 func Init(scene string, configOptions ...ConfigOption) error {
+	initDefaultController()
 	defaultController.DrawStack = render.GlobalDrawStack
 	defaultController.logicHandler = event.DefaultBus
 	return defaultController.Init(scene, configOptions...)
 }
 
 func AddScene(name string, sc scene.Scene) error {
+	initDefaultController()
 	return defaultController.AddScene(name, sc)
 }
 
 func IsDown(key string) bool {
+	initDefaultController()
 	return defaultController.IsDown(key)
 }
 
 func IsHeld(key string) (bool, time.Duration) {
+	initDefaultController()
 	return defaultController.IsHeld(key)
 }
 
 func SetUp(key string) {
+	initDefaultController()
 	defaultController.SetUp(key)
 }
 
 func SetDown(key string) {
+	initDefaultController()
 	defaultController.SetDown(key)
 }
 
 func SetViewportBounds(rect intgeom.Rect2) {
+	initDefaultController()
 	defaultController.SetViewportBounds(rect)
 }
 
 func ShiftScreen(x, y int) {
+	initDefaultController()
 	defaultController.ShiftScreen(x, y)
 }
 
 func SetScreen(x, y int) {
+	initDefaultController()
 	defaultController.SetScreen(x, y)
 }
 
 func SetFullScreen(fs bool) error {
+	initDefaultController()
 	return defaultController.SetFullScreen(fs)
 }
 
 func SetBorderless(bs bool) error {
+	initDefaultController()
 	return defaultController.SetBorderless(bs)
 }
 
 func ScreenShot() *image.RGBA {
+	initDefaultController()
 	return defaultController.ScreenShot()
 }
 
 func SetLoadingRenderable(r render.Renderable) {
+	initDefaultController()
 	defaultController.SetLoadingRenderable(r)
 }
 
 func SetBackground(b Background) {
+	initDefaultController()
 	defaultController.SetBackground(b)
 }
 
 func SetColorBackground(img image.Image) {
+	initDefaultController()
 	defaultController.SetColorBackground(img)
 }
 
 func GetBackgroundImage() image.Image {
+	initDefaultController()
 	return defaultController.GetBackgroundImage()
 }
 
 func Width() int {
+	initDefaultController()
 	return defaultController.Width()
 }
 
 func Height() int {
+	initDefaultController()
 	return defaultController.Height()
 }
 
 func HideCursor() error {
+	initDefaultController()
 	return defaultController.HideCursor()
 }
