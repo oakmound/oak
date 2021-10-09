cc21cbf245ee37940b8c288b2474f2554b78b0b4
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-10-09 07:32:05 -0500

    debugtools: respect input context for DebugMouseRelease

--- debugtools/mouse.go
+++ debugtools/mouse.go
@@ -10,7 +10,7 @@ import (
 // DebugMouseRelease will print the position and button pressed of the mouse when the mouse is released, if the given
 // key is held down at the time. If no key is given, it will always be printed
 func DebugMouseRelease(ctx *scene.Context, k string) {
-	event.GlobalBind(mouse.Release, func(_ event.CID, ev interface{}) int {
+	ctx.EventHandler.GlobalBind(mouse.Release, func(_ event.CID, ev interface{}) int {
 		mev, _ := ev.(*mouse.Event)
 		if k == "" || ctx.KeyState.IsDown(k) {
 			dlog.Info(mev)
