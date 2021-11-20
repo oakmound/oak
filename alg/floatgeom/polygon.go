e26aedd9fba1640e13c6ef24f13b0f72e5ce5a71
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-11-20 11:44:25 -0600

    alg/floatgeom: add polygon projection and containment helpers

--- alg/floatgeom/polygon.go
+++ alg/floatgeom/polygon.go
@@ -1,5 +1,9 @@
 package floatgeom
 
+import (
+	"github.com/oakmound/oak/v3/alg"
+)
+
 // A Polygon2 is a series of points in 2D space.
 type Polygon2 struct {
 	// Bounding is a cached bounding box calculated from the input points
@@ -7,7 +11,8 @@ type Polygon2 struct {
 	Bounding Rect2
 	// The component points of the polygon. If modified, Bounding should
 	// be updated with NewBoundingRect2.
-	Points []Point2
+	Points      []Point2
+	rectangular bool
 }
 
 // NewPolygon2 is a helper method to construct a valid polygon. Polygons
@@ -16,8 +21,9 @@ func NewPolygon2(p1, p2, p3 Point2, pn ...Point2) Polygon2 {
 	pts := append([]Point2{p1, p2, p3}, pn...)
 	bounding := NewBoundingRect2(pts...)
 	return Polygon2{
-		Bounding: bounding,
-		Points:   pts,
+		Bounding:    bounding,
+		Points:      pts,
+		rectangular: isRectangular(pts...),
 	}
 }
 
@@ -81,5 +87,101 @@ func getSide(a, b Point2) int {
 		return -1
 	} else {
 		return 1
+	}
+}
+
+// OverlappingRectCollides returns whether a Rect2 intersects or is contained by this Polygon.
+// This method differs from RectCollides because it assumes that we already know r overlaps with pg.Bounding.
+func (pg Polygon2) OverlappingRectCollides(r Rect2) bool {
+	if pg.rectangular {
+		return true
+	}
+	diags := [][2]Point2{
+		{
+			{r.Min.X(), r.Max.Y()},
+			{r.Max.X(), r.Min.Y()},
+		}, {
+			r.Min,
+			r.Max,
+		},
+	}
+	last := pg.Points[len(pg.Points)-1]
+	for i := 0; i < len(pg.Points); i++ {
+		next := pg.Points[i]
+		if r.Contains(next) {
+			return true
+		}
+		// Checking line segment from last to next
+		for _, diag := range diags {
+			if orient(diag[0], diag[1], last) != orient(diag[0], diag[1], next) &&
+				orient(next, last, diag[0]) != orient(next, last, diag[1]) {
+				return true
+			}
+		}
+
+		last = next
+	}
+	return false
+}
+
+// RectCollides returns whehter a Rect2 intersects or is contained by this Polygon
+func (pg Polygon2) RectCollides(r Rect2) bool {
+	x := float64(r.Min.X())
+	y := float64(r.Min.Y())
+	x2 := float64(r.Max.X())
+	y2 := float64(r.Max.Y())
+
+	dx := pg.Bounding.Min.X()
+	dy := pg.Bounding.Min.Y()
+	dx2 := pg.Bounding.Max.X()
+	dy2 := pg.Bounding.Max.Y()
+
+	dimOverlap := false
+	if x > dx {
+		if x < dx2 {
+			dimOverlap = true
+		}
+	} else {
+		if dx < x2 {
+			dimOverlap = true
+		}
+	}
+	if y > dy {
+		if y < dy2 {
+			dimOverlap = true
+		}
+	} else {
+		if dy < y2 {
+			dimOverlap = true
+		}
+	}
+	if !dimOverlap {
+		return false
+	}
+	return pg.OverlappingRectCollides(r)
+}
+
+func isRectangular(pts ...Point2) bool {
+	last := pts[len(pts)-1]
+	for _, pt := range pts {
+		// The last point needs to share an x or y value with this point
+		if !alg.F64eq(pt.X(), last.X()) && !alg.F64eq(pt.Y(), last.Y()) {
+			return false
+		}
+		last = pt
+	}
+	return true
+}
+
+func orient(p1, p2, p3 Point2) int8 {
+	val := (p2.Y()-p1.Y())*(p3.X()-p2.X()) -
+		(p2.X()-p1.X())*(p3.Y()-p2.Y())
+	switch {
+	case val < 0:
+		return 2
+	case val > 0:
+		return 1
+	default:
+		return 0
 	}
 }
