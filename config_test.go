a007b823c259c2f856fc3c74c8fbd3126f8ebf4c
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-12-04 08:37:15 -0600

    oak: fix config test for driver field
    This reveals this additional field is a breaking change on equality comparisons. I think this is minor enough to not worry about.

--- config_test.go
+++ config_test.go
@@ -17,7 +17,7 @@ func TestDefaultConfigFileMatchesEmptyConfig(t *testing.T) {
 		t.Fatalf("failed to load default.config (1): %v", err)
 	}
 	c2, _ := NewConfig()
-	if c1 != c2 {
+	if !configEquals(c1, c2) {
 		t.Fatalf("config from file did not match default config (1)")
 	}
 	f, err := os.Open(filepath.Join("testdata", "default.config"))
@@ -31,9 +31,76 @@ func TestDefaultConfigFileMatchesEmptyConfig(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to load config data from reader")
 	}
-	if c2 != c3 {
+	if !configEquals(c2, c3) {
 		t.Fatalf("config from reader did not match default config (1)")
 	}
+}
+
+func configEquals(c1, c2 Config) bool {
+	type comparableConfig struct {
+		Assets              Assets           `json:"assets"`
+		Debug               Debug            `json:"debug"`
+		Screen              Screen           `json:"screen"`
+		BatchLoadOptions    BatchLoadOptions `json:"batchLoadOptions"`
+		FrameRate           int              `json:"frameRate"`
+		DrawFrameRate       int              `json:"drawFrameRate"`
+		IdleDrawFrameRate   int              `json:"idleDrawFrameRate"`
+		Language            string           `json:"language"`
+		Title               string           `json:"title"`
+		EventRefreshRate    Duration         `json:"refreshRate"`
+		BatchLoad           bool             `json:"batchLoad"`
+		GestureSupport      bool             `json:"gestureSupport"`
+		LoadBuiltinCommands bool             `json:"loadBuiltinCommands"`
+		TrackInputChanges   bool             `json:"trackInputChanges"`
+		EnableDebugConsole  bool             `json:"enableDebugConsole"`
+		TopMost             bool             `json:"topmost"`
+		Borderless          bool             `json:"borderless"`
+		Fullscreen          bool             `json:"fullscreen"`
+		SkipRNGSeed         bool             `json:"skip_rng_seed"`
+	}
+	cc1 := comparableConfig{
+		Assets:              c1.Assets,
+		Debug:               c1.Debug,
+		Screen:              c1.Screen,
+		BatchLoadOptions:    c1.BatchLoadOptions,
+		FrameRate:           c1.FrameRate,
+		DrawFrameRate:       c1.DrawFrameRate,
+		IdleDrawFrameRate:   c1.IdleDrawFrameRate,
+		Language:            c1.Language,
+		Title:               c1.Title,
+		EventRefreshRate:    c1.EventRefreshRate,
+		BatchLoad:           c1.BatchLoad,
+		GestureSupport:      c1.GestureSupport,
+		LoadBuiltinCommands: c1.LoadBuiltinCommands,
+		TrackInputChanges:   c1.TrackInputChanges,
+		EnableDebugConsole:  c1.EnableDebugConsole,
+		TopMost:             c1.TopMost,
+		Borderless:          c1.Borderless,
+		Fullscreen:          c1.Fullscreen,
+		SkipRNGSeed:         c1.SkipRNGSeed,
+	}
+	cc2 := comparableConfig{
+		Assets:              c2.Assets,
+		Debug:               c2.Debug,
+		Screen:              c2.Screen,
+		BatchLoadOptions:    c2.BatchLoadOptions,
+		FrameRate:           c2.FrameRate,
+		DrawFrameRate:       c2.DrawFrameRate,
+		IdleDrawFrameRate:   c2.IdleDrawFrameRate,
+		Language:            c2.Language,
+		Title:               c2.Title,
+		EventRefreshRate:    c2.EventRefreshRate,
+		BatchLoad:           c2.BatchLoad,
+		GestureSupport:      c2.GestureSupport,
+		LoadBuiltinCommands: c2.LoadBuiltinCommands,
+		TrackInputChanges:   c2.TrackInputChanges,
+		EnableDebugConsole:  c2.EnableDebugConsole,
+		TopMost:             c2.TopMost,
+		Borderless:          c2.Borderless,
+		Fullscreen:          c2.Fullscreen,
+		SkipRNGSeed:         c2.SkipRNGSeed,
+	}
+	return cc1 == cc2
 }
 
 func TestConfig_overwriteFrom(t *testing.T) {
