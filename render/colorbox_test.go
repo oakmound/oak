1602befa08ca8be4b57346cde920007d713c8d3d
Patrick Stephen <patrick.d.stephen@gmail.com>
2021-12-11 12:47:53 -0600

    render: add property based testing for color box R

--- render/colorbox_test.go
+++ render/colorbox_test.go
@@ -1,8 +1,10 @@
 package render
 
 import (
+	"image"
 	"image/color"
 	"testing"
+	"testing/quick"
 )
 
 func TestColorBox(t *testing.T) {
@@ -19,4 +21,36 @@ func TestColorBox(t *testing.T) {
 	if rgba.At(6, 6) != (color.RGBA{0, 0, 0, 0}) {
 		t.Fatalf("rgba exceeded w/h")
 	}
+}
+
+func TestColorBoxR(t *testing.T) {
+	if err := quick.Check(testColorBoxRProperties, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// w and h are int8 because int16 can make us check 65536*65536 = 4 billion pixels which can time out
+func testColorBoxRProperties(r, g, b, a uint8, w8, h8 int8) bool {
+	c := color.RGBA{r, g, b, a}
+	w := int(w8)
+	h := int(h8)
+	sp := NewColorBoxR(w, h, c)
+	w2, h2 := sp.GetDims()
+	if w2 != w {
+		return false
+	}
+	if h2 != h {
+		return false
+	}
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
+	sp.Draw(img, 0, 0)
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			cAt := img.RGBAAt(x, y)
+			if c != cAt {
+				return false
+			}
+		}
+	}
+	return true
 }
