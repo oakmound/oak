cd9925aafde4d83372b7c1df0b785b1ec4dbf17d
Patrick Stephen <patrick.d.stephen@gmail.com>
2022-03-06 12:12:52 -0600

    event: add persistent binding support

--- event/bind.go
+++ event/bind.go
@@ -13,6 +13,30 @@ func (eb *Bus) Bind(name string, callerID CID, fn Bindable) {
 	eb.pendingMutex.Unlock()
 }
 
+// PersistentBind acts like Bind, but persists the binding such that if the event
+// bus is reset, the binding will still trigger. Thes bindings should likely be global
+// bindings, using a CID of 0, or be tolerant to the CID bound not being present after
+// such a clear.
+func (eb *Bus) PersistentBind(name string, callerID CID, fn Bindable) {
+	eb.pendingMutex.Lock()
+	opt := UnbindOption{
+		Event: Event{
+			Name:     name,
+			CallerID: callerID,
+		}, Fn: fn}
+	eb.binds = append(eb.binds, opt)
+	eb.persistentBinds = append(eb.persistentBinds, opt)
+	eb.pendingMutex.Unlock()
+}
+
+// ClearPersistentBindings removes all persistent bindings. It will not unbind them
+// from the bus, but they will not be bound following the next bus reset. 
+func (eb *Bus) ClearPersistentBindings() {
+	eb.pendingMutex.Lock()
+	eb.persistentBinds = []UnbindOption{}
+	eb.pendingMutex.Unlock()
+}
+
 // GlobalBind binds on the bus to the cid 0, a non entity.
 func (eb *Bus) GlobalBind(name string, fn Bindable) {
 	eb.Bind(name, 0, fn)
