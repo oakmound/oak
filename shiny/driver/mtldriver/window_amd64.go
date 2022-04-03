38f3cc494627c2f0605f46ba8303729d3975b61a
Patrick Stephen <patrick@strongdm.com>
2022-04-03 12:55:17 -0500

    shiny/driver/mtldriver: fix compilation of intel color variant

--- shiny/driver/mtldriver/window_amd64.go
+++ shiny/driver/mtldriver/window_amd64.go
@@ -3,20 +3,30 @@
 
 package mtldriver
 
+import (
+	"image"
+	"image/color"
+
+	"github.com/oakmound/oak/v3/shiny/driver/internal/drawer"
+	"github.com/oakmound/oak/v3/shiny/screen"
+	"golang.org/x/image/draw"
+	"golang.org/x/image/math/f64"
+)
+
 func (w *windowImpl) Upload(dp image.Point, src screen.Image, sr image.Rectangle) {
-	draw.Draw(w.rgba, sr.Sub(sr.Min).Add(dp), src.RGBA(), sr.Min, draw.Src)
+	draw.Draw(w.bgra, sr.Sub(sr.Min).Add(dp), src.RGBA(), sr.Min, draw.Src)
 }
 
 func (w *windowImpl) Fill(dr image.Rectangle, src color.Color, op draw.Op) {
-	draw.Draw(w.rgba, dr, &image.Uniform{src}, image.Point{}, op)
+	draw.Draw(w.bgra, dr, &image.Uniform{src}, image.Point{}, op)
 }
 
 func (w *windowImpl) Draw(src2dst f64.Aff3, src screen.Texture, sr image.Rectangle, op draw.Op) {
-	draw.NearestNeighbor.Transform(w.rgba, src2dst, src.(*textureImpl).rgba, sr, op, nil)
+	draw.NearestNeighbor.Transform(w.bgra, src2dst, src.(*textureImpl).rgba, sr, op, nil)
 }
 
 func (w *windowImpl) DrawUniform(src2dst f64.Aff3, src color.Color, sr image.Rectangle, op draw.Op) {
-	draw.NearestNeighbor.Transform(w.rgba, src2dst, &image.Uniform{src}, sr, op, nil)
+	draw.NearestNeighbor.Transform(w.bgra, src2dst, &image.Uniform{src}, sr, op, nil)
 }
 
 func (w *windowImpl) Copy(dp image.Point, src screen.Texture, sr image.Rectangle, op draw.Op) {
