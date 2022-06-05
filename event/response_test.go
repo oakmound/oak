8f7e7e036ea7db42cbe5c00863cc815fdd88f6d2
implausiblyfun <cyreal123@gmail.com>
2022-06-05 12:37:19 -0400

    throwaway event test for illustration

--- event/response_test.go
+++ event/response_test.go
@@ -1,6 +1,7 @@
 package event_test
 
 import (
+	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -11,10 +12,17 @@ import (
 func TestBindingResponses(t *testing.T) {
 	t.Run("UnbindThisBinding", func(t *testing.T) {
 		b := event.NewBus(event.NewCallerMap())
-
+		li := 1
 		var calls int32
+		var innerCalls int32
+		var b2 event.Binding
 		b1 := b.UnsafeBind(1, 0, func(ci event.CallerID, h event.Handler, i interface{}) event.Response {
 			atomic.AddInt32(&calls, 1)
+			b2 = b.UnsafeBind(2, ci.CID(), func(ci event.CallerID, h event.Handler, i interface{}) event.Response {
+				atomic.AddInt32(&innerCalls, 1)
+				return event.ResponseUnbindThisBinding
+			})
+			li++
 			return event.ResponseUnbindThisBinding
 		})
 		<-b1.Bound
@@ -28,6 +36,12 @@ func TestBindingResponses(t *testing.T) {
 		if calls != 1 {
 			t.Fatal(expectedError("calls", 1, calls))
 		}
+		<-b2.Bound
+		<-b.Trigger(2, nil)
+		time.Sleep(1 * time.Second)
+		<-b.Trigger(2, nil)
+		t.Fatal(fmt.Errorf("%d", innerCalls))
+		t.Fatal(fmt.Errorf("%d", li))
 	})
 	t.Run("UNbindThisCaller", func(t *testing.T) {
 		b := event.NewBus(event.NewCallerMap())
